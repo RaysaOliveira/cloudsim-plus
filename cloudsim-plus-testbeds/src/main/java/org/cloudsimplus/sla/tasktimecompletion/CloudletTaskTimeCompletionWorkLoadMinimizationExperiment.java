bb97f03db795283ae77e6048b33419658406e639
raysaoliveira <oliveira.raysa@gmail.com>
2017-07-03 18:25:02 +0100

    Priority Worst Fit Decreasing changed

--- cloudsim-plus-testbeds/src/main/java/org/cloudsimplus/sla/tasktimecompletion/CloudletTaskTimeCompletionWorkLoadMinimizationExperiment.java
+++ cloudsim-plus-testbeds/src/main/java/org/cloudsimplus/sla/tasktimecompletion/CloudletTaskTimeCompletionWorkLoadMinimizationExperiment.java
@@ -276,6 +276,9 @@ public class CloudletTaskTimeCompletionWorkLoadMinimizationExperiment extends Si
      * Selects a VM to run a Cloudlet that will minimize the Cloudlet response
      * time.
      *
+     * Is a modified implementation of the Worst Fit Decreasing Algorithm.
+     * We called Priority Worst Fit Decreasing
+     *
      * @param cl the Cloudlet to select a VM to
      * @return the selected Vm
      */
